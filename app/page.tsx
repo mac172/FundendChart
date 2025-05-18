6f32c15832b831a6ad31259dece3ff7f8ff925ce
digvijay164 <digvijaydesai164@gmail.com>
2025-05-18 11:28:50 +0530

    jde

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ import { TextFade } from "@/components/section3/TextFade";
 
 
 export default function Home() {
-   const whyRef = useRef<HTMLDivElement>(null);
-   const [showWhy, setShowWhy] = useState(false);
-   const [showWhyCount, setShowWhyCount] = useState(0);
+  const whyRef = useRef<HTMLDivElement>(null);
+  const [showWhy, setShowWhy] = useState(false);
+  const [showWhyCount, setShowWhyCount] = useState(0);
 
   const scrollToWhy = () => {
     if (whyRef.current) {
@@ -23,39 +23,71 @@ export default function Home() {
     <div>
       {/* Hero Section */}
       <div className="sticky top-0">
-        <HeroSection onGetFundedClick={scrollToWhy}/>
+        <HeroSection onGetFundedClick={scrollToWhy} />
       </div>
       {/* Section 2 */}
       <div className="w-full h-screen">
-       <WhySection />
+        <WhySection />
       </div>
       {/* Section 3 */}
       <div className="relative w-full h-screen bg-black">
         {/* head-text section */}
         <div className="head-text">
-        <MarqueTextAnimation 
-          text="Swipe free for 28 pairs and all metals!"
-        />
+          <MarqueTextAnimation
+            text="Swipe free for 28 pairs and all metals!"
+          />
         </div>
 
         <div className="section-2 h-[90%] gap-[1rem] flex items-center justify-center bg-black">
           <div className="container-1 h-[60%] w-[47%]  bg-red-600">
-            <h1>DEMO TEXT</h1>
+            <video src="/section3Graph"></video>
           </div>
-          <div className="container-2 h-[60%] w-[47%]  bg-red-700">
-          <TextFade
-            direction="up"
-            staggerChildren={0.1}
-            className="h-[100%] w-[100%] bg-amber-500 flex items-center justify-center"
-          >
-            {/* Add your content here */}
-            <span>Trade on forex and market in capital</span>
-          </TextFade>
+
+          <div className="container-2 h-[80%] w-[47%]  ">
+            <TextFade
+              direction="up"
+              staggerChildren={0.1}
+              className="h-[100%] w-[100%] flex-col p-[2rem] text-[20px] items-center justify-cener"
+            >
+              <motion.div
+                className="w-fit mb-[1rem]"
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.2, ease: "easeOut", delay: 0.2 }}
+              >
+                <div className="inline-flex  px-4 py-2 rounded-full bg-gray-800 border border-gray-700 shadow-lg shadow-blue-500/20 ">
+                  <span className="text-gray-200 uppercase text-sm font-semibold tracking-wide">
+                    What is FXology?
+                  </span>
+                </div>
+              </motion.div>
+              {/* Add your content here */}
+              <div className="mb-[1rem]">
+                <span className="text-[2rem]">Trade on forex and other</span>
+                <span className="text-[2rem]">market in capital</span>
+                <span className="text-[2rem]">upto 640,000 USD!</span>
+              </div>
+
+              <div className="mb-[1rem]">
+                <span>We provide unique trading programs for Forex traders, based </span>
+                <span>upon which we search for the best options to work together with.</span>
+                <span>We provide you with Training accounts that you can use to trade</span>
+                <span>and earn commission without the risk of losing your own funds!</span>
+              </div>
+
+              <div className="">
+                <span>You are presented with a choice of the widest variety of</span>
+                <span>training programs on the market - it is up to you to choose</span>
+                <span>based on your experience and preference. Whether you are a </span>
+                <span>rookie, advanced or experienced trader, we are certain</span>
+                <span>that you will find the most perfectly suited progrma</span>
+              </div>
+            </TextFade>
           </div>
         </div>
 
       </div>
-       {/* <motion.div
+      {/* <motion.div
         ref={whyRef}
         initial={{ scale: 0.95, opacity: 0 }}
         whileInView={{ scale: 1, opacity: 1 }}
